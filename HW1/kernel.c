f1821b38e95821a429eb1967e271fae4faf0006f
Tate-W <tatenelson2003@gmail.com>
2024-09-19 14:56:02 -0500

    Desktop file update

--- HW1/kernel.c
+++ HW1/kernel.c
@@ -1,9 +1,14 @@
-#include "libos.a"
-int clear_scr(int start_row, int start_col, int width, int height);
+#include "libos.h"
+
+int box(unsigned int srow, unsigned int scol, unsigned int erow, unsigned int ecol);
+// int clear_scr(int start_row, int start_col, int width, int height);
 
 int main()
 {
 
-
+    box(21, 49, 27, 79);
+    print_to(24, 59, "Hello World!");
     return 0;
+
 }
+
